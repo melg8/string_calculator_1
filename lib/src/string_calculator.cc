9cd09581251315af94b013480541a94afcd48ffc
melg8 <public.melg8@gmail.com>
2017-04-25 23:10:09 +0300

    Add initial setup files

---
+++ lib/src/string_calculator.cc
@@ -0,0 +1,6 @@
+#include <string_calculator.h>
+
+int StringCalculator::Add(const std::string& numbers) {
+  (void)numbers;
+  return -1;
+}
---
+++ lib/src/string_calculator.h
@@ -0,0 +1,6 @@
+#include <string>
+
+class StringCalculator {
+ public:
+  int Add(const std::string& numbers);
+};
